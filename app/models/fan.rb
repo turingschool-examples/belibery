69aa39f0bdc2713203d06b59365977e710e2e0d9
Rachel Warbelow <rachelwarbelow@gmail.com>
2015-10-29 12:00:22 -0600

    updated at noon

--- app/models/fan.rb
+++ app/models/fan.rb
@@ -1,7 +1,20 @@
 class Fan < ActiveRecord::Base
-  validates :name, presence: true
-  validates :email, presence: true, 
-                    uniqueness: true
+  validates :name, presence: true,
+                   format: { with: /\A[a-zA-Z ]+\z/, 
+                             message: "can only contain letters"}
 
-  # validates_presence_of :name
+  validates :email, presence: true, 
+                    uniqueness: true,
+                    length: { in: 5..50 }
+
+  validate :no_tories
+  belongs_to :location
+  
+  def nickname
+    "#{name}lieber"
+  end
+
+  def no_tories
+    errors.add(:name, "cannot be torie") if name == "torie" || name == "Torie"
+  end
 end
--- test/models/fan_test.rb
+++ test/models/fan_test.rb
@@ -39,4 +39,40 @@ class FanTest < ActiveSupport::TestCase
 
     assert_equal 1, result.count
   end
+
+  test "fan name only contains letters" do
+    fan = Fan.new(name: "Ro55 Edfort",
+                  email: "truebelieber@gmail.com",
+                  location_id: 1)
+
+    refute fan.valid?
+  end
+
+  test "fan email must be between 5 and 50 characters" do
+    long_email_fan = Fan.new(name: "Ross Edfort",
+                             email: "abcdeabcdeababcdeabcdeababcdeabcdeababcdeabcdeabcdeabcdeabcde@yahoo.com",
+                             location_id: 1)
+    short_email_fan = Fan.new(name: "Ross Edfort",
+                              email: "abcd",
+                              location_id: 1)
+
+    refute long_email_fan.valid?
+    refute short_email_fan.valid?
+  end
+
+  test "fan cannot be named torie" do
+    fan = Fan.new(name: "Torie",
+                  email: "truebelieber@gmail.com",
+                  location_id: 1)
+
+    refute fan.valid?
+  end
+
+  test "fan has a belieber nickname" do
+    fan = Fan.new(valid_attributes)
+
+    assert_equal "Ross Edfortlieber", fan.nickname
+  end
+
+  should belong_to(:location)
 end
